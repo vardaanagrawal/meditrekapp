9ed2df3b79c3fe35501185cddad4f4552f5a6caf
Vardaan <vardaanagrawal517@gmail.com>
2022-02-09 10:44:13 +0530

    -

--- frontend/src/components/UserAuth/Home.js
+++ frontend/src/components/UserAuth/Home.js
@@ -6,8 +6,7 @@ export default function Home() {
   return (
     <div className="homePage">
       <div className="box">
-          <div className="title">DIGIREX</div>
-          An app that helps you to maintain your personalised medical record.
+          <div className="title">MEDITREK</div>
           <div className="btns">
               <Link to='/login'>Log In</Link>
               <Link to='/signup'>Sign Up</Link>
